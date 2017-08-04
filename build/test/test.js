3845be97307486390d0166a7a9f89a47c825b811
Nikita Zakharov <nikitarui@mail.ru>
2017-08-04 19:18:37 +0300

    1

---
+++ build/test/test.js
@@ -0,0 +1,17 @@
+var assert = require('chai').assert;
+describe('Main Test', function () {
+    this.timeout(10000);
+    this.slow(2000);
+    it('test 1', function (done) {
+        setTimeout(function () {
+            console.log("test 1");
+            done();
+        }, 5000);
+    });
+    it('tets 2', function () {
+        console.log("test 2");
+    });
+    it('tets 2', function () {
+        console.log("test 2");
+    });
+});
