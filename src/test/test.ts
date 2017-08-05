5ff396212e808e3c60971ed2bd736aa6f65297c2
Nikita Zakharov <nikitarui@mail.ru>
2017-08-05 18:38:45 +0300

    changed

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,30 +1,50 @@
-var assert = require('chai').assert;
+var assert = require('chai').should();
 
-describe('Main Test', function(){
+describe('People Test', function(){
 
     this.timeout(10000);
-    this.slow(2000);
+    this.slow(1000);
 
-    it('test 1', function(done){
+    class People{
+        get age(): number {
+            return this._age;
+        }
 
-        setTimeout(function(){
-            console.log("test 1");
+        set age(value: number) {
+            this._age = value;
+        }
+
+        get name(): string {
+            return this._name;
+        }
+
+        set name(value: string) {
+            this._name = value;
+        }
+
+        private _age:number = 0;
+        private _name:string = '';
+
+    }
+
+    var p = new People();
+
+    it('should be name equals Vasya', function(done){
+
+        setTimeout(()=>{
+            p.name = "Vasya";
+            p.name.should.be.equal('Vasya');
             done();
-        }, 5000);
+        }, 2000);
 
     });
 
-    it('tets 2', function(){
-        console.log("test 2");
+    it('should be age equals 24', function(){
+        p.name = "Vasya";
+        p.age = 23;
+        p.age.should.be.equal(23);
     });
 
-    it('tets 2', function(){
-        console.log("test 2");
-    });
-
-    it('test3', function(){
-        console.log("test");
-    })
 
 })
 
